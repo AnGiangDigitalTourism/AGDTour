908972a47cbce54b5647325386fa6a259e2e784c
AnGiangDigitalTourism <agdigitaltour@gmail.com>
2023-09-24 06:36:43 +0700

    goodmorning

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ app.get("/places", (req, res) => {
 
 app.get("/places/artifacts", async (req, res) => {
   const arList = await bacton.find();
+  console.log(arList);
   res.render("artifacts", { fetchData: arList });
 });
 
